e5a338961c43b52dd62866ce6dd4650a0922ee59
KrishnaRavichandranD <krishnaravi6494@gmail.com>
2021-10-29 11:36:06 +0530

    venki

---
+++ src/org/all/LanguageClass.java
@@ -0,0 +1,22 @@
+package org.all;
+
+import org.tamil.Tamil;
+
+public class LanguageClass extends Tamil {
+	private void allLanguage() {
+		System.out.println("all language:tamil,english,telugu");
+	
+
+}public static void main(String[] args) {
+	LanguageClass lc=new LanguageClass();
+	lc.allLanguage();
+	lc.tamilLanguage();
+	lc.englishLanguage();
+	lc.teluguLanguage();
+	
+}
+	
+	
+}
+
+
